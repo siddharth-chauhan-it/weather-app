a5b20adc6ec459fff674ef5a56e532333903caf1
siddharth-chauhan-it <siddharthchauhan.it@gmail.com>
2022-02-21 13:56:23 +0530

    Home Page changes

---
+++ src/components/WebsiteContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WebsiteContainer = ({ children }) => {
+  return <div className="mx-auto max-w-[1536px]">{children}</div>;
+};
+
+export default WebsiteContainer;
